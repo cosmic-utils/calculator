e8806c452871bcc00d43ada9d7163ddbaa1bb4c2
voedipus <voedipus@gmail.com>
2026-03-01 00:28:23 +0500

    fix qalc output parsing

--- src/app.rs
+++ src/app.rs
@@ -461,7 +461,9 @@ impl Application for CosmicCalculator {
                     return Task::batch(tasks);
                 }
 
-                let outcome = String::from_utf8(output.stdout).unwrap_or_default();
+                let outcome = String::from_utf8(output.stdout)
+                    .unwrap_or_default()
+                    .replace(['\n', '\r'], "");
                 if outcome.is_empty() {
                     tracing::error!("Failed to parse qalc output");
                     tasks.push(self.update(Message::ShowToast(
@@ -470,7 +472,7 @@ impl Application for CosmicCalculator {
                     return Task::batch(tasks);
                 };
 
-                self.calculator.outcome = outcome.to_string();
+                self.calculator.outcome = outcome.clone();
 
                 let mut history = self.config.history.clone();
                 history.push(self.calculator.clone());
@@ -487,7 +489,7 @@ impl Application for CosmicCalculator {
                     .text(self.calculator.expression.clone())
                     .data(self.calculator.clone());
 
-                self.calculator.expression = outcome.to_string();
+                self.calculator.expression = outcome;
             }
             Message::Key(modifiers, key) => {
                 for (key_bind, action) in &self.key_binds {
